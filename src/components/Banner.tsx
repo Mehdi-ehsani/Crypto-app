984b06d447315bd0bd76290af9fc7013a7cf4962
Mahdi Ehsani <metti.work@gmail.com>
2024-02-02 17:19:30 +0330

    create banner css file and add classnames

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,4 @@
+// image
 import bitcoinImg from "../assets/Bitcoin.png";
 import etheriumImg from "../assets/Etherium.png";
 import liteCoinImg from "../assets/LiteCoin.png";
@@ -7,21 +8,24 @@ import star2Img from "../assets/star-img-2.png";
 import star3Img from "../assets/star-img-3.png";
 import star4Img from "../assets/star-img-4.png";
 
+// css
+import styles from "../styles/banner.module.css";
+
 const Banner = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <h1>We make crypto clear and simple</h1>
         <p>Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to every trader at every level.</p>
-        <button>Get Started</button>
-        <div>
-           <img src={bitcoinImg} alt="coinImg" />
-           <img src={moneroImg} alt="coinImg" />
-           <img src={liteCoinImg} alt="coinImg" />
-           <img src={etheriumImg} alt="coinImg" />
-           <img src={star1Img} alt="coinImg" />
-           <img src={star2Img} alt="coinImg" />
-           <img src={star3Img} alt="coinImg" />
-           <img src={star4Img} alt="coinImg" />
+        <button className={styles.btn}>Get Started</button>
+        <div className={styles.imageContainer}>
+           <img className={styles.coin} src={bitcoinImg} alt="coinImg" />
+           <img className={styles.coin} src={moneroImg} alt="coinImg" />
+           <img className={styles.coin} src={liteCoinImg} alt="coinImg" />
+           <img className={styles.coin} src={etheriumImg} alt="coinImg" />
+           <img className={styles.coin} src={star1Img} alt="coinImg" />
+           <img className={styles.coin} src={star2Img} alt="coinImg" />
+           <img className={styles.coin} src={star3Img} alt="coinImg" />
+           <img className={styles.coin} src={star4Img} alt="coinImg" />
         </div>
     </div>
   )
