6a320c1e4452b5fdb46eb902dd5832a70406cf17
Mahdi Ehsani <metti.work@gmail.com>
2024-02-03 16:17:23 +0330

    add details to the banner.css file

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,25 +12,39 @@ import star4Img from "../assets/star-img-4.png";
 import styles from "../styles/banner.module.css";
 
 const Banner = () => {
-  return (
-    <div className={styles.container}>
-        <h1>We make crypto<br/>clear and simple</h1>
-        <p>Buy, sell, and grow your crypto with CoinFlip, the platform dedicated<br/> to every trader at every level.</p>
-        <button className={styles.btn}>Get Started</button>
-        <div className={styles.imageContainer}>
-           <img className={styles.coin} src={bitcoinImg} alt="coinImg" />
-           <img className={styles.coin} src={moneroImg} alt="coinImg" />
-           <img className={styles.coin} src={liteCoinImg} alt="coinImg" />
-           <img className={styles.coin} src={etheriumImg} alt="coinImg" />
-           <img className={styles.coin} src={star1Img} alt="coinImg" />
-           <img className={styles.coin} src={star2Img} alt="coinImg" />
-           <img className={styles.coin} src={star3Img} alt="coinImg" />
-           <img className={styles.coin} src={star4Img} alt="coinImg" />
-           <div className={styles.circleOne}></div>
-           <div className={styles.circleTwo}></div>
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.textContainer}>
+				<h1>
+					We make crypto
+					<br />
+					clear and simple
+				</h1>
+				<p>
+					Buy, sell, and grow your crypto with CoinFlip, the platform dedicated
+					<br /> to every trader at every level.
+				</p>
+				<button className={styles.btn}>Get Started</button>
+			</div>
 
-export default Banner
+			<div className={styles.imageContainer}>
+				<img className={styles.coin} src={bitcoinImg} alt="coinImg" />
+				<img className={styles.coin} src={moneroImg} alt="coinImg" />
+				<img className={styles.coin} src={liteCoinImg} alt="coinImg" />
+				<img className={styles.coin} src={etheriumImg} alt="coinImg" />
+				<img className={styles.coin} src={star1Img} alt="coinImg" />
+				<img className={styles.coin} src={star2Img} alt="coinImg" />
+				<img className={styles.coin} src={star3Img} alt="coinImg" />
+				<img className={styles.coin} src={star4Img} alt="coinImg" />
+				<div className={styles.circleOne}></div>
+				<div className={styles.circleTwo}></div>
+				<div className={styles.lineOne}></div>
+				<div className={styles.lineTwo}></div>
+				<div className={styles.lineThree}></div>
+        
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
