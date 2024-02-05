0cc0ab05be2c58556a292d2ed0eddc148fa5289a
sina <sinaderik7@gmail.com>
2024-02-05 19:00:48 +0330

    not found page updated

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,8 +11,7 @@ const NotFound = () => {
 				className=" h-13 absolute top-3/4 left-1/2 translate-x-[-50%] 
         		z-2 px-4 py-3 rounded-md bg-btn-404 hover:scale-[103%] 
 		 		ease-in duration-300 border-2 
-		 		border-white font-semiboldbold"
-			>
+		 		border-white font-semiboldbold">
 				Back to Home
 			</Link>
 =======
