cce43dc8438b517d01e63e113d5c44b38426b2b3
sina <sinaderik7@gmail.com>
2024-02-06 15:15:15 +0330

    feat: light mode got imporoved and mui loading component add to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Banner from "./components/Banner"
 import Cards from "./components/Cards/Cards"
 import { useDataContext } from '../src/context'
 import { Link } from "react-router-dom"
-import Loading from "./components/Loading"
+// import Loading from "./components/Loading"
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function App() {
   const { data, loading , isDark , setTheme } = useDataContext()
@@ -11,7 +12,9 @@ export default function App() {
   function RenderContent() {
     if (loading) {
       return (
-        <Loading />
+        <div className="flex items-center justify-center">
+          <CircularProgress className="mt-10" />
+        </div>
       )
     } else {
       return (
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -10,40 +10,42 @@ export default function Cards() {
     const { isDark } = useDataContext()
 
     return (
-        <div className='container mx-auto'>
-            <div className='card-wrapper flex flex-col md:flex-row items-center justify-center gap-8 px-4 md:px-1'>
-                <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
-                    <img src={cardImg1} alt="card-img" />
-                    <h1 className={isDark?"text-3xl font-bold mt-5 mb-4":"textLight text-3xl font-bold mt-5 mb-4"}>Trade Desk</h1>
-                    <p className={isDark?"text-slate-400 mt-4 mb-4":"textLight mt-4 mb-4"}>Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform</p>
-                    <div className={isDark?"cardLinkDark card-link__wrapper flex items-center justify-center":"cardLinkLight card-link__wrapper flex items-center justify-center"}>
-                        <a className="card-link mr-3" href="#">
-                            Get Started
-                        </a>
-                        <FaArrowRight className="card-arrow-icon" />
+        <div className={isDark?"bodyDark":"bodyLight"}>
+            <div className={isDark ? "bodyDark container mx-auto" : "bodyLight container mx-auto pt-10 pb-10"}>
+                <div className='card-wrapper flex flex-col md:flex-row items-center justify-center gap-8 px-4 md:px-1'>
+                    <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
+                        <img src={cardImg1} alt="card-img" />
+                        <h1 className={isDark ? "text-3xl font-bold mt-5 mb-4" : "textLight text-3xl font-bold mt-5 mb-4"}>Trade Desk</h1>
+                        <p className={isDark ? "text-slate-400 mt-4 mb-4" : "textLight mt-4 mb-4"}>Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform</p>
+                        <div className={isDark ? "cardLinkDark card-link__wrapper flex items-center justify-center" : "cardLinkLight card-link__wrapper flex items-center justify-center"}>
+                            <a className="card-link mr-3" href="#">
+                                Get Started
+                            </a>
+                            <FaArrowRight className="card-arrow-icon" />
+                        </div>
                     </div>
-                </div>
-                <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
-                    <img src={cardImg2} alt="card-img" />
-                    <h1 className={isDark?"text-3xl font-bold mt-5 mb-4":"textLight text-3xl font-bold mt-5 mb-4"}>CoinFlip ATMs</h1>
-                    <p className={isDark?"text-slate-400 mt-4 mb-4":"textLight mt-4 mb-4"}>We have thousands of ATMs located across the U.S. where you can easily convert cash to crypto</p>
-                    <div className={isDark?"cardLinkDark card-link__wrapper flex items-center justify-center":"cardLinkLight card-link__wrapper flex items-center justify-center"}>
-                        <a className="card-link mr-3" href="#">
-                            Find an ATM
-                        </a>
-                        <FaArrowRight className="card-arrow-icon" />
+                    <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
+                        <img src={cardImg2} alt="card-img" />
+                        <h1 className={isDark ? "text-3xl font-bold mt-5 mb-4" : "textLight text-3xl font-bold mt-5 mb-4"}>CoinFlip ATMs</h1>
+                        <p className={isDark ? "text-slate-400 mt-4 mb-4" : "textLight mt-4 mb-4"}>We have thousands of ATMs located across the U.S. where you can easily convert cash to crypto</p>
+                        <div className={isDark ? "cardLinkDark card-link__wrapper flex items-center justify-center" : "cardLinkLight card-link__wrapper flex items-center justify-center"}>
+                            <a className="card-link mr-3" href="#">
+                                Find an ATM
+                            </a>
+                            <FaArrowRight className="card-arrow-icon" />
+                        </div>
                     </div>
-                </div>
-                <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
-                    <img src={cardImg3} alt="card-img" />
-                    <h1 className={isDark?"text-3xl font-bold mt-5 mb-4":"textLight text-3xl font-bold mt-5 mb-4"}>CoinFlip Wallet</h1>
-                    <p className={isDark?"text-slate-400 mt-4 mb-4":"textLight mt-4 mb-4"}>Store your growing investments in our
-                        non-custodial wallet that gives you access to a full suite of DeFi services in one place</p>
-                    <div className={isDark?"cardLinkDark card-link__wrapper flex items-center justify-center":"cardLinkLight card-link__wrapper flex items-center justify-center"}>
-                        <a className="card-link mr-3" href="#">
-                            Download the App
-                        </a>
-                        <FaArrowRight className="card-arrow-icon" />
+                    <div className={isDark ? "cardDark card  text-center pb-4 pr-4 pl-4" : "cardLight card  text-center pb-4 pr-4 pl-4"}>
+                        <img src={cardImg3} alt="card-img" />
+                        <h1 className={isDark ? "text-3xl font-bold mt-5 mb-4" : "textLight text-3xl font-bold mt-5 mb-4"}>CoinFlip Wallet</h1>
+                        <p className={isDark ? "text-slate-400 mt-4 mb-4" : "textLight mt-4 mb-4"}>Store your growing investments in our
+                            non-custodial wallet that gives you access to a full suite of DeFi services in one place</p>
+                        <div className={isDark ? "cardLinkDark card-link__wrapper flex items-center justify-center" : "cardLinkLight card-link__wrapper flex items-center justify-center"}>
+                            <a className="card-link mr-3" href="#">
+                                Download the App
+                            </a>
+                            <FaArrowRight className="card-arrow-icon" />
+                        </div>
                     </div>
                 </div>
             </div>
